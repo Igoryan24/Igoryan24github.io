ea19990572ca7b1572689cd8c5646803734ead0d
Igoryan <ihor.matsiieskiy@gmail.com>
2021-01-27 20:33:54 +0200

    added slider and items js

--- app.js
+++ app.js
@@ -59,3 +59,138 @@ likeButtons.forEach((btn) => {
         this.classList.toggle("liked");
     })
 })
+
+// change product-count 
+
+let decrementBtns = document.querySelectorAll(".decrement-button");
+let incrementBtns = document.querySelectorAll(".increment-button");
+let quantityValue = document.querySelectorAll(".product-quantity input");
+
+/*console.log(decrementBtns)
+console.log(incrementBtns)
+console.log(quantityValue)*/
+/*let currentCount = +quantityValue.value;*/
+let minCount = 1;
+let maxCount = 10;
+
+/*for(let i = 0;i < quantityValue.length;i++) {
+    let currentCount = +quantityValue[i].value;
+    toggleButtonState(currentCount,decrementBtns[i],incrementBtns[i]);
+}*/
+
+/*quantityValue.forEach(() => {
+    let currentCount = +quantityValue.value;
+    toggleButtonState(currentCount,decrementBtns,incrementBtns);
+    console.log(quantityValue)
+});
+
+function toggleButtonState(count,decrementBtn,incrementBtn) {
+    decrementBtn.disabled = count <= minCount;
+    incrementBtn.disabled = count >= maxCount;
+}*/
+
+/*toggleDecrementState(currentCount);
+
+function toggleDecrementStata(count) {
+    if(count <= 1) {
+        decrementBtns.disabled = true;
+    } else {
+        decrementBtns.disabled = false;
+    }
+}
+toggleDecrementState(currentCount);*/
+
+/*for(let i = 0;i < incrementBtns.length;i++) {
+    incrementBtns[i].addEventListener("click",function () {
+        let currentCount = +quantityValue[i].value;
+        let nextCount = currentCount + 1;
+        quantityValue[i].value = nextCount;
+       
+        toggleButtonState(nextCount,decrementBtns[i],incrementBtns[i]);
+    })
+}
+
+for(let i = 0;i < decrementBtns.length;i++) {
+    decrementBtns[i].addEventListener("click",function () {
+        let currentCount = +quantityValue[i].value;
+        let nextCount = currentCount - 1;
+        quantityValue[i].value = nextCount;
+        
+        toggleButtonState(nextCount,decrementBtns[i],incrementBtns[i]);
+        
+    })
+}*/
+/*incrementBtns.forEach((incrementBtns) => {
+    incrementBtns.addEventListener("click", function () {
+        let currentCount = +quantityValue.value;
+        let nextCount = currentCount + 1;
+        quantityValue.value = nextCount;
+        
+        toggleButtonState(nextCount,decrementBtns[i],incrementBtns[i]);
+        console.log(incrementBtns)
+    })
+})
+
+decrementBtns.forEach((decrementBtns) => {
+    decrementBtns.addEventListener("click", function() {
+        let currentCount = +quantityValue.value;
+        let nextCount = currentCount - 1;
+        quantityValue.value = nextCount;
+    
+        toggleButtonState(nextCount,decrementBtns,incrementBtns);
+        console.log(decrementBtns)
+    })
+})*/
+
+    //function constructor change product quantity
+
+    function Counter(incrementBtn,decrementBtn,inputField,minCount = 1,maxCount = 10) {
+
+        this.domRefs = {
+            incrementBtn,
+            decrementBtn,
+            inputField,
+        }
+        this.toggleButtonState = function () {
+            let count = this.domRefs.inputField.value;
+            this.domRefs.decrementBtn.disabled = count <= minCount;
+            this.domRefs.incrementBtn.disabled = count >= maxCount;
+        }
+        this.toggleButtonState();
+
+        this.increment = function() {
+            let currentCount = +this.domRefs.inputField.value;
+            let nextCount = currentCount + 1;
+            this.domRefs.inputField.value = nextCount;
+        
+            this.toggleButtonState()
+        }
+
+        this.decrement = function() {
+            let currentCount = +this.domRefs.inputField.value;
+            let nextCount = currentCount - 1;
+            this.domRefs.inputField.value = nextCount;
+        
+            this.toggleButtonState()
+        }
+        
+        this.domRefs.incrementBtn.addEventListener("click", this.increment.bind(this));
+        this.domRefs.decrementBtn.addEventListener("click", this.decrement.bind(this));
+        
+    }
+
+    for(let i = 0;i < quantityValue.length;i++) {
+        const counter = new Counter(incrementBtns[i],decrementBtns[i],quantityValue[i]); 
+    }
+    /*quantityValue.forEach((quantityValue) => {
+        const counter = new Counter(incrementBtns,decrementBtns,quantityValue); 
+    });*/
+
+    // Slider 
+    $(".slider-block").slick({
+        dots:true,
+        arrows: true,
+    });
+
+
+
